cf57937de0190dac01dfd79b62013b087d3664d5
gitlep1 <antonioshivers2500@yahoo.com>
2022-11-03 17:13:35 -0400

    added header navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { route, Router } from "react-router-dom";
+
+import Navbar from "./components/navbar/navbar";
+
+const App = () => {
+  return (
+    <section>
+      <Navbar />
+    </section>
+  );
+};
+
+export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <header id="navbarSection">
+      <h1 id="portfolioTitle">Portfolio</h1>
+
+      <ul id="navLinks">
+        <li>Home</li>
+        <li>Projects</li>
+        <li></li>
+      </ul>
+    </header>
+  );
+};
+
+export default Navbar;
